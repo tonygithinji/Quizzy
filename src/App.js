28d0fd753e746826897a5aa99f36fa5fd30b0914
Anthony Githinji <tonygithinji94@gmail.com>
2020-05-21 12:06:21 +0300

    Initial commit & project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-white text-gray-700 border-2 border-gray-400 rounded-lg mt-2 text-center">
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
